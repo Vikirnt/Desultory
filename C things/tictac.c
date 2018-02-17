9237d24fadb72d8e12759fb8851fc7f5d658ce6b
vikirnt <vikrantgajria@gmail.com>
2018-02-17 22:47:15 +0530

    Added tictactoe.c

---
+++ C things/tictac.c
@@ -0,0 +1,130 @@
+/*
+ * Made in Turbo C... because that's what we're taught in Mumbai University.
+ * Yeah fuck that. But we made it in 1 hr.
+ */
+
+#include <stdio.h>
+
+#define PLAY_X 'X'
+#define PLAY_O 'O'
+#define BLANK  '-'
+#define FILLED 'V'
+
+// Grid shared with all functions.
+static char grid [3][3];
+
+// Struct to store values together.
+struct pos {
+	int x;
+        int y;
+};
+
+// Clear the grid.
+void empty () {
+	int i, j;
+        for (i = 0; i < 3; i++) {
+        	for (j = 0; j < 3; j++) {
+	        	grid [i][j] = BLANK;
+                }
+        }
+}
+
+// Display the grid.
+void display () {
+	int i, j;
+        for (i = 0; i < 3; i++) {
+        	for (j = 0; j < 3; j++)
+              		printf ("%c ", grid[i][j]);
+                printf ("\n");
+        }
+}
+
+// Start the game.
+int start () {
+        int d;
+	printf ("\n\n\n***PRESS 1 TO START OR 0 TO EXIT***\n");
+        scanf ("%d", &d);
+        return d;
+}
+
+// Check end-game conditions.
+int check () {
+	int i, j;
+        for (i = 0; i < 3; i++) {
+        	// Check rows.
+        	if (grid[i][0] == grid[i][1] && grid[i][1] == grid[i][2])
+                	return grid[i][0];
+                else
+                // Check columns.
+                if (grid[0][i] == grid[1][i] && grid[1][i] == grid[2][i])
+                	return grid[0][i];
+        }
+       	// Check diagonals.
+        if (grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2])
+        	return grid[0][0];
+        else
+	if (grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0])
+        	return grid[2][0];
+        // Check for blanks.
+        for (i = 0; i < 3; i++)
+        	for (j = 0; j < 3; j++)
+                	if (grid[i][j] == BLANK)
+                        	return BLANK;
+
+        // If all conditions fail then draw.
+        return FILLED;
+}
+
+// Check if the choords are valid.
+int checkcoords (int x, int y) {
+	if (x == -1 || y == -1)
+        	return 0;
+	else if (grid [x][y] != BLANK)
+        	return 1;
+        else
+        	return 0;
+}
+
+struct pos input (int player) {
+        int x, y;
+        struct pos coords;
+        do {
+		printf ("PLAYER %c -> (x, y)", player);
+        	scanf  ("%d %d", &x, &y);
+        } while (checkcoords (x-1, y-1));
+        coords.x = x-1;
+        coords.y = y-1;
+        return coords;
+}
+
+int main () {
+	int alt, res;
+        struct pos coords;
+
+        while (start ()) {
+        	// Clear grid.
+                empty ();
+
+                // Alternator.
+                alt = PLAY_X;
+		while (check () == BLANK) {
+                        display ();
+                        coords = input (alt);
+                        if (coords.x == -1) return -1;
+                        grid [coords.x][coords.y] = alt;
+                        alt = alt == PLAY_X ? PLAY_O : PLAY_X;
+
+                        res = check ();
+                        printf ("%c\n", res);
+                        if (res == FILLED)
+                        	printf ("DRAW");
+                        else if (res != BLANK) {
+                        	display ();
+                        	printf ("WINNER IS %c!", res);
+                        }
+                        else continue;
+                }
+        }
+
+        return 0;
+}
